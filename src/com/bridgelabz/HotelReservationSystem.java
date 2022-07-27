c2de15c8f205ebca6c759ff1820ef8347bc606ac
nagarajnsg1995 <nagarajsbg023@gmail.com>
2022-07-27 09:37:05 +0530

    Added: To finding the cheapest hotal in list of hotels

--- src/com/bridgelabz/HotelReservationSystem.java
+++ src/com/bridgelabz/HotelReservationSystem.java
@@ -3,8 +3,14 @@ package com.bridgelabz;
 public class HotelReservationSystem {
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Reservation System");
-        HotelSetup hotelSetup=new HotelSetup();
+        HotelSetup hotelSetup = new HotelSetup();
         hotelSetup.addHotel();
+        hotelSetup.showHotelInfo();
 
+
+        System.out.println(" ");
+        System.out.println("Cheapest Hotel Based on WeekdayRegularRate");
+        hotelSetup.findCheapestHotelOne("01-Jan-2021", "05-Jan-2021");
+        System.out.println(" ");
     }
 }
--- src/com/bridgelabz/HotelSetup.java
+++ src/com/bridgelabz/HotelSetup.java
@@ -1,22 +1,49 @@
 package com.bridgelabz;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Optional;
 
 public class HotelSetup {
-
     ArrayList<Hotel> myHotelList = new ArrayList();
 
     public void addHotel() {
 
         Hotel LakeWood = new Hotel("LakeWood", 110, 80, 90, 80, 3);
-        Hotel BridgeWood = new Hotel("BridgeWood", 150, 110, 50, 50, 4);
+        Hotel BridgeWood = new Hotel("Hampi", 150, 110, 50, 50, 4);
         Hotel RidgeWood = new Hotel("RidgeWood", 220, 100, 150, 40, 5);
 
         myHotelList.add(LakeWood);
         myHotelList.add(BridgeWood);
         myHotelList.add(RidgeWood);
+ //       System.out.println(myHotelList);
+    }
 
-        System.out.println(myHotelList);
+    public void showHotelInfo() {
+        for (int i = 0; i < myHotelList.size(); i++) {
+            System.out.println(myHotelList.get(i));
+        }
+    }
+
+
+    public void findCheapestHotelOne(String startDateRange, String endDateRange) {
+
+        LocalDate startDate = LocalDate.parse(startDateRange, DateTimeFormatter.ofPattern("dd-MMM-yyyy"));
+        LocalDate endDate = LocalDate.parse(endDateRange, DateTimeFormatter.ofPattern("dd-MMM-yyyy"));
+
+        int numberOfDays = endDate.getDayOfMonth() - startDate.getDayOfMonth() + 1;
+
+        Optional<Hotel> cheapestHotel = this.myHotelList.stream().sorted(Comparator.comparing(Hotel::getWeekdayRegularRate)).findFirst();
+
+        Hotel hotel = new Hotel();
+        hotel.setHotelName(cheapestHotel.get().getHotelName());
+        hotel.setTotal(cheapestHotel.get().getWeekdayRegularRate() * numberOfDays);
+        System.out.println("HotelName :" + hotel.getHotelName());
+        System.out.println("NumberOfDaysStayed  WeekdayRegularRate :" + hotel.getTotal() + "$");
 
     }
+
+
 }
