c07b4fa9466f265ac10047ae67364814512f9ed0
nagarajnsg1995 <nagarajsbg023@gmail.com>
2022-07-27 09:03:40 +0530

    Added: welcome message to hotel reservation system

---
+++ src/com/bridgelabz/HotelReservationSystem.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class HotelReservationSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation System");
+        HotelSetup hotelSetup=new HotelSetup();
+        hotelSetup.addHotel();
+
+    }
+}
---
+++ src/com/bridgelabz/HotelSetup.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+
+public class HotelSetup {
+
+    ArrayList<Hotel> myHotelList = new ArrayList();
+
+    public void addHotel() {
+
+        Hotel LakeWood = new Hotel("LakeWood", 110, 80, 90, 80, 3);
+        Hotel BridgeWood = new Hotel("BridgeWood", 150, 110, 50, 50, 4);
+        Hotel RidgeWood = new Hotel("RidgeWood", 220, 100, 150, 40, 5);
+
+        myHotelList.add(LakeWood);
+        myHotelList.add(BridgeWood);
+        myHotelList.add(RidgeWood);
+
+        System.out.println(myHotelList);
+
+    }
+}
